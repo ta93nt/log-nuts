edcb97d03525b095b9879093c0056345873df33e
Taku <hiatana72@gmail.com>
2020-02-15 19:39:32 +0900

    Bootstrapを導入し、トップページを作成

--- LogNuts/settings.py
+++ LogNuts/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4', #レスポンシブデザイン導入のため
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'builtins':[ 
+                'bootstrap4.templatetags.bootstrap4', #レスポンシブデザイン対応
             ],
         },
     },
--- lognutsapp/urls.py
+++ lognutsapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'lognuts'
+
 urlpatterns = [
      path('', views.TopView.as_view(), name='top'),
 ]
