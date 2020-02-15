126bfd9466be6ca4106883eea8ec736da77b555c
Taku <hiatana72@gmail.com>
2020-02-15 19:09:56 +0900

    lognutsappの登録、urls.pyの作成、トップページの作成

--- LogNuts/settings.py
+++ LogNuts/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lognutsapp.apps.LognutsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
