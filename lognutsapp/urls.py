22b9c4a2a7b48639ce7fbe15fce430c14686ccf2
Taku <hiatana72@gmail.com>
2020-02-16 15:53:16 +0900

    マイページにカレンダーを追加

--- lognutsapp/urls.py
+++ lognutsapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('mypage/<int:pk>/', views.MypageView.as_view(), name='mypage'),
+    path('mypage/<int:pk>/<int:year>/<int:month>/', views.MypageView.as_view(), name='mypage'),
     path('search_input/<int:pk>/', views.SearchInput.as_view(), name='search_input'),
     path('search_complete/<int:pk>/<int:id>/', views.SearchComplete.as_view(), name='search_complete'),
 ]
--- lognutsapp/views.py
+++ lognutsapp/views.py
@@ -28,6 +28,9 @@ from .forms import (
 #ライブラリ
 import pandas as pd
 from decimal import Decimal, ROUND_HALF_UP #Decimal変換の際に利用
+import calendar
+import datetime
+from collections import deque
 
 
 """
@@ -40,16 +43,113 @@ class OnlyYouMixin(UserPassesTestMixin):
         user = self.request.user
         return user.pk == int(self.kwargs['pk']) or user.is_superuser
 
+
+class BaseCalendarMixin:
+    """カレンダー関連Mixinの、基底クラス"""
+    first_weekday = 0  # 0は月曜から、1は火曜から。6なら日曜日からになります。お望みなら、継承したビューで指定してください。
+    week_names = ['月', '火', '水', '木', '金', '土', '日']  # これは、月曜日から書くことを想定します。['Mon', 'Tue'...
+
+    def setup_calendar(self):
+        self._calendar = calendar.Calendar(self.first_weekday)
+
+    def get_week_names(self):
+        """first_weekday(最初に表示される曜日)にあわせて、week_namesをシフトする"""
+        week_names = deque(self.week_names)
+        week_names.rotate(-self.first_weekday)  # リスト内の要素を右に1つずつ移動...なんてときは、dequeを使うと中々面白いです
+        return week_names
+
+class MonthCalendarMixin(BaseCalendarMixin):
+    """月間カレンダーの機能を提供するMixin"""
+
+    def get_previous_month(self, date):
+        """前月を返す"""
+        if date.month == 1:
+            return date.replace(year=date.year-1, month=12, day=1)
+        else:
+            return date.replace(month=date.month-1, day=1)
+
+    def get_next_month(self, date):
+        """次月を返す"""
+        if date.month == 12:
+            return date.replace(year=date.year+1, month=1, day=1)
+        else:
+            return date.replace(month=date.month+1, day=1)
+
+    def get_month_days(self, date):
+        """その月の全ての日を返す"""
+        return self._calendar.monthdatescalendar(date.year, date.month)
+
+    def get_current_month(self):
+        """現在の月を返す"""
+        month = self.kwargs.get('month')
+        year = self.kwargs.get('year')
+        if month and year:
+            month = datetime.date(year=int(year), month=int(month), day=1)
+        else:
+            month = datetime.date.today().replace(day=1)
+        return month
+
+    def get_month_calendar(self):
+        """月間カレンダー情報の入った辞書を返す"""
+        self.setup_calendar()
+        current_month = self.get_current_month()
+        calendar_data = {
+            'now': datetime.date.today(),
+            'month_days': self.get_month_days(current_month),
+            'month_current': current_month,
+            'month_previous': self.get_previous_month(current_month),
+            'month_next': self.get_next_month(current_month),
+            'week_names': self.get_week_names(),
+        }
+        return calendar_data
+
+class WeekCalendarMixin(BaseCalendarMixin):
+    """週間カレンダーの機能を提供するMixin"""
+    def get_week_days(self):
+        """その週の日を全て返す"""
+        month = self.kwargs.get('month')
+        year = self.kwargs.get('year')
+        day = self.kwargs.get('day')
+        if month and year and day:
+            date = datetime.date(year=int(year), month=int(month), day=int(day))
+        else:
+            date = datetime.date.today()
+            print('----dateはこちら{}----'.format(date))
+        for week in self._calendar.monthdatescalendar(date.year, date.month):
+            if date in week:  # 週ごとに取り出され、中身は全てdatetime.date型。該当の日が含まれていれば、それが今回表示すべき週です
+                return week
+    def get_week_calendar(self):
+        """週間カレンダー情報の入った辞書を返す"""
+        self.setup()
+        days = self.get_week_days()
+        first = days[0]
+        last = days[-1]
+        calendar_data = {
+            'now': datetime.date.today(),
+            'days': days,
+            'previous': first - datetime.timedelta(days=7),
+            'next': first + datetime.timedelta(days=7),
+            'week_names': self.get_week_names(),
+            'first': first,
+            'last': last,
+        }
+        return calendar_data
+
 class TopView(generic.TemplateView):
     """Lognutsトップページ"""
     template_name = 'lognuts/top.html' 
 
-class MypageView(OnlyYouMixin, generic.TemplateView):
+class MypageView(OnlyYouMixin, MonthCalendarMixin, generic.TemplateView):
     """Lognutsマイページ"""
     model = User
     template_name = 'lognuts/mypage.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        context['PersonalLog'] = PersonalLog.objects.values('date', 'food_name').filter(
+            user=self.request.user
+        )
         return context
 
 class Login(LoginView):
