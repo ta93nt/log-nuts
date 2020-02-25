e07c5e2656de14bc87d23d0b59294a59b5dbd5f6
Taku <hiatana72@gmail.com>
2020-02-26 00:14:12 +0900

    settings.py更新

--- LogNuts/settings.py
+++ LogNuts/settings.py
@@ -128,7 +128,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
