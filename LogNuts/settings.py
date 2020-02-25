4c93a064ea92f3c26db37f61ae7a3abd78351637
Taku <hiatana72@gmail.com>
2020-02-25 22:06:22 +0900

    local_setting.pyを追加

--- LogNuts/settings.py
+++ LogNuts/settings.py
@@ -28,7 +28,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
 
 ALLOWED_HOSTS = []
 
