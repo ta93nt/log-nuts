39dc55eedefbbdbd73d9154177e9a58c8a60542f
Taku <hiatana72@gmail.com>
2020-02-25 23:19:30 +0900

    whitenoiseを更新

--- LogNuts/settings.py
+++ LogNuts/settings.py
@@ -126,7 +126,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
